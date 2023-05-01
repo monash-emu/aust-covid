b2c979f20d2f25ae8e30a7ba4fd9581c1eb0dc7a
James Trauer <james.trauer@monash.edu>
2023-05-01 21:54:35 +1000

    Change name of sheet in Excel seems to stop lots of warnings about default import settings for openpyxl

--- aust_covid/model.py
+++ aust_covid/model.py
@@ -54,7 +54,12 @@ def load_pop_data() -> tuple:
     for group in range(16):
         skip_rows += list(range(228 + group * 6, 233 + group * 6))
     sheet_name = "31010do002_202206.xlsx"
-    data = pd.read_excel(DATA_PATH / sheet_name, sheet_name="Table_7", skiprows=skip_rows, index_col=[0])
+    data = pd.read_excel(
+        DATA_PATH / sheet_name, 
+        sheet_name="Table_7", 
+        skiprows=skip_rows, 
+        index_col=[0],
+    ) 
     return data, sheet_name
 
 
@@ -68,7 +73,7 @@ def get_uk_pop_data():
     sheet_name = "cens_01nscbirth__custom_6028079_page_spreadsheet.xlsx"
     data = pd.read_excel(
         DATA_PATH / sheet_name, 
-        sheet_name="Sheet 1", 
+        sheet_name="Sheet_1", 
         skiprows=list(range(0, 11)) + list(range(30, 37)), 
         usecols="B:C", 
         index_col=0,
