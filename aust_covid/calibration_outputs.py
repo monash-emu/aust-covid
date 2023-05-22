52f98b016d245e65cf4d181d52dfb2a3f512d914
James Trauer <james.trauer@monash.edu>
2023-05-22 21:05:39 +1000

    Reorganise locations of output functions

--- aust_covid/calibration_outputs.py
+++ aust_covid/calibration_outputs.py
@@ -3,16 +3,31 @@ import arviz as az
 from arviz.labels import MapLabeller
 from pathlib import Path
 import pandas as pd
+import numpy as np
 import datetime
 import plotly.graph_objects as go
+import plotly.express as px
 import matplotlib as mpl
 
 from estival.model import BayesianCompartmentalModel
 
-from aust_covid.output_utils import convert_idata_to_df, run_samples_through_model, plot_from_model_runs_df, round_sigfig
-
 BASE_PATH = Path(__file__).parent.parent.resolve()
 SUPPLEMENT_PATH = BASE_PATH / "supplement"
+
+
+def round_sigfig(
+    value: float, 
+    sig_figs: int
+) -> float:
+    """
+    Round a number to a certain number of significant figures, 
+    rather than decimal places.
+    
+    Args:
+        value: Number to round
+        sig_figs: Number of significant figures to round to
+    """
+    return round(value, -int(np.floor(np.log10(value))) + (sig_figs - 1))
 
 
 def get_fixed_param_value_text(
@@ -83,6 +98,41 @@ def get_prior_dist_support(
         The bounds to the prior's distribution joined together
     """
     return " to ".join([str(i) for i in prior.bounds()])
+
+
+def convert_idata_to_df(
+    idata: az.data.inference_data.InferenceData, 
+    param_names: list,
+) -> pd.DataFrame:
+    """
+    Convert arviz inference data to dataframe organised
+    by draw and chain through multi-indexing.
+    
+    Args:
+        idata: arviz inference data
+        param_names: String names of the model parameters
+    """
+    sampled_idata_df = idata.to_dataframe()[param_names]
+    return sampled_idata_df.sort_index(level="draw").sort_index(level="chain")
+
+
+def run_samples_through_model(
+    samples_df: pd.DataFrame, 
+    model: BayesianCompartmentalModel,
+    output: str,
+) -> pd.DataFrame:
+    """
+    Run parameters dataframe in format created by convert_idata_to_df
+    through epidemiological model to get outputs of interest.
+    
+    Args:
+        samples_df: Parameters to run through in format generated from convert_idata_to_df
+        model: Model to run them through
+    """
+    sres = pd.DataFrame(index=model.model._get_ref_idx(), columns=samples_df.index)
+    for (chain, draw), params in samples_df.iterrows():
+        sres[(chain,draw)] = model.run(params.to_dict()).derived_outputs[output]
+    return sres
 
 
 def graph_param_progression(
@@ -157,6 +207,41 @@ def graph_sampled_outputs(
     fig = plot_from_model_runs_df(sample_model_results, sampled_df, prior_names, start_date, end_date)
     fig.add_trace(go.Scatter(x=target_data.index, y=target_data, marker=dict(color="black"), name=output, mode="markers"))
     return fig
+
+
+def plot_from_model_runs_df(
+    model_results: pd.DataFrame, 
+    sampled_df: pd.DataFrame,
+    param_names: list,
+    start_date: datetime.datetime,
+    end_date: datetime.datetime,
+) -> go.Figure:
+    """
+    Create interactive plot of model outputs by draw and chain
+    from standard data structures.
+    
+    Args:
+        model_results: Model outputs generated from run_samples_through_model
+        sampled_df: Inference data converted to dataframe in output format of convert_idata_to_df
+    """
+    melted = model_results.melt(ignore_index=False)
+    melted.columns = ["chain", "draw", "notifications"]
+
+    # Add parameter values from sampled dataframe to plotting 
+    for (chain, draw), params in sampled_df.iterrows():
+        for p in param_names:
+            melted.loc[(melted["chain"]==chain) & (melted["draw"] == draw), p] = round_sigfig(params[p], 3)
+        
+    plot = px.line(
+        melted, 
+        y="notifications", 
+        color="chain", 
+        line_group="draw", 
+        hover_data=melted.columns,
+        labels={"index": ""},
+    )
+    plot.update_xaxes(range=(start_date, end_date))
+    return plot
 
 
 def tabulate_parameters(parameters, param_units, priors, param_descriptions, param_evidence):
