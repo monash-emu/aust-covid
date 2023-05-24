efa868eb16e83bf1fae6646187d0e21bfa54a899
James Trauer <james.trauer@monash.edu>
2023-05-24 22:09:38 +1000

    Split out model utils to new file

--- aust_covid/model.py
+++ aust_covid/model.py
@@ -1,75 +1,19 @@
-import pylatex as pl
 from datetime import datetime
 import numpy as np
 import pandas as pd
 import plotly.express as px
 from pathlib import Path
-from jax import numpy as jnp
-from jax import scipy as jsp
 
 from summer2 import CompartmentalModel, Stratification, StrainStratification
-from summer2.parameters import Parameter, DerivedOutput, Function, Time, Data
+from summer2.parameters import Parameter, DerivedOutput, Function, Time
 
+from aust_covid.model_utils import triangle_wave_func, convolve_probability, gamma_cdf, build_gamma_dens_interval_func
 from aust_covid.inputs import load_pop_data, load_uk_pop_data
 
 
 BASE_PATH = Path(__file__).parent.parent.resolve()
 SUPPLEMENT_PATH = BASE_PATH / "supplement"
 DATA_PATH = BASE_PATH / "data"
-
-
-def triangle_wave_func(
-    time: float, 
-    start: float, 
-    duration: float, 
-    peak: float,
-) -> float:
-    """
-    Generate a peaked triangular wave function
-    that starts from and returns to zero.
-
-    Args:
-        time: Model time
-        start: Time at which wave starts
-        duration: Duration of wave
-        peak: Peak flow rate for wave
-    Returns:
-        The wave function
-    """
-    gradient = peak / (duration * 0.5)
-    peak_time = start + duration * 0.5
-    time_from_peak = jnp.abs(peak_time - time)
-    return jnp.where(time_from_peak < duration * 0.5, peak - time_from_peak * gradient, 0.0)
-
-
-def convolve_probability(source_output, density_kernel):
-    return jnp.convolve(source_output, density_kernel)[:len(source_output)]
-
-
-def gamma_cdf(
-    shape: float, 
-    mean: float, 
-    x: jnp.array,
-) -> jnp.array:
-    """
-    The regularised gamma function is the CDF of the gamma distribution
-    (which is referred to by scipy as "gammainc")
-
-    Args:
-        shape: Shape parameter to the desired gamma distribution
-        mean: Expectation of the desired gamma distribution
-        x: Values to calculate the result over
-
-    Returns:
-        Array of CDF values corresponding to input request (x)
-    """
-    return jsp.special.gammainc(shape, x * shape / mean)
-
-
-def build_gamma_dens_interval_func(shape, mean, model_times):
-    lags = Data(model_times - model_times[0])
-    cdf_values = Function(gamma_cdf, [shape, mean, lags])
-    return Function(jnp.gradient, [cdf_values])
 
 
 """
