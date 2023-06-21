e39c1a50f10d5b41bd5f346cdf6365054572277a
James Trauer <james.trauer@monash.edu>
2023-06-21 22:48:14 +1000

    Move all general code to a separate folder from application code

--- aust_covid/inputs.py
+++ aust_covid/inputs.py
@@ -101,35 +101,3 @@ def load_google_mob_year_df(year=int) -> pd.DataFrame:
     mob_df = mob_df[mob_cols]
     mob_df.index = pd.to_datetime(mob_df.index)
     return mob_df
-
-
-def load_param_info(
-    data_path: Path, 
-    parameters: dict,
-) -> pd.DataFrame:
-    """
-    Load specific parameter information from 
-    a ridigly formatted yaml file or crash otherwise.
-
-    Args:
-        data_path: Location of the source file
-        parameters: The parameters provided by the user (with their values)
-
-    Returns:
-        The parameters info DataFrame contains the following fields:
-            descriptions: A brief reader-digestible name/description for the parameter
-            units: The unit of measurement for the quantity (empty string if dimensionless)
-            evidence: TeX-formatted full description of the evidence underpinning the choice of value
-    """
-    data_cols = ["descriptions", "units", "evidence"]
-    param_keys = parameters.keys()
-    out_df = pd.DataFrame(index=param_keys, columns=data_cols)
-    with open(data_path, "r") as param_file:
-        all_data = yaml.safe_load(param_file)
-        for col in data_cols:
-            working_data = all_data[col]
-            if param_keys != working_data.keys():
-                raise ValueError("Incorrect keys for data")
-            out_df[col] = out_df.index.map(working_data)
-        out_df["manual_values"] = parameters.values()
-    return out_df
