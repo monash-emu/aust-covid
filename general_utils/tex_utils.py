5433b4d34d647659056d73bac2c5d53f53ef4ef7
jtrauer <james.trauer@monash.edu>
2023-08-05 10:51:49 +1000

    Add horizontal table lines

--- general_utils/tex_utils.py
+++ general_utils/tex_utils.py
@@ -73,6 +73,7 @@ class StandardTexDoc(TexDoc):
         self.add_line('\\usepackage{hyperref}', 'preamble')
         self.add_line('\\usepackage{graphicx}', 'preamble')
         self.add_line('\\usepackage{longtable}', 'preamble')
+        self.add_line('\\usepackage{booktabs}', 'preamble')
         self.add_line('\\graphicspath{ {./images/} }', 'preamble')
         self.add_line(f'\\addbibresource{{{self.bib_filename}.bib}}', 'preamble')
         self.add_line(f'\\title{{{self.title}}}', 'preamble')
