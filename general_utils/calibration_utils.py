02af59c64d64cbbda545a437ba03dad27ea03037
jtrauer <james.trauer@monash.edu>
2023-08-07 20:35:58 +1000

    Make sure table writing working with a different table

--- general_utils/calibration_utils.py
+++ general_utils/calibration_utils.py
@@ -27,7 +27,7 @@ def round_sigfig(
         value: Number to round
         sig_figs: Number of significant figures to round to
     """
-    return round(value, -int(np.floor(np.log10(value))) + (sig_figs - 1))
+    return round(value, -int(np.floor(np.log10(value))) + (sig_figs - 1)) if value != 0.0 else 0.0
 
 
 def param_table_to_tex(
