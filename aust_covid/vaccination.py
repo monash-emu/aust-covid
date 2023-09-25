73e1580bb8a2fc101bfeed13b6819fe05697266c
James Trauer <james.trauer@monash.edu>
2023-09-25 15:32:52 +1000

    Change dictionary names in masks dictionaries to be more human-readable

--- aust_covid/vaccination.py
+++ aust_covid/vaccination.py
@@ -7,38 +7,38 @@ def get_vacc_data_masks(
     df: pd.DataFrame,
 ) -> Dict[str, List[str]]:
     masks = {
-        'age_16_2dose': [
+        'age 16+, 2+ doses': [
             c for c in df.columns if 'Number of people fully vaccinated' in c and 
             not any(s in c for s in [' - M - ', ' - F - '])
         ],
-        'age_16_3dose': [
+        'age 16+, 3+ doses': [
             col for col in df.columns if 
             'National' in col and 
             any([s in col for s in ['who have received 3 doses', 'with 3 or more doses', 'with more than two doses', 'with more than 2 doses']]) and 
             any([s in col for s in ['16', '18']]) and
             not any([s in col for s in ['increase', 'Percentage', 'Indigenous']])
         ],
-        'age_16_4dose': [
+        'age 16+, 4+ doses': [
             col for col in df.columns if
             'National' in col and
             any([s in col for s in ['Winter Boosters number', 'who have received 4 doses', 'Fourth dose number']]) and
             not any([s in col for s in ['30', '65', 'increase']])
         ],
-        'age_12_15_2dose': [
+        'age 12-15, 2+ doses': [
             col for col in df.columns if
             '12-15' in col and
             any([s in col for s in ['National', 'Age group']]) and
             any([s in col for s in ['2 doses', 'fully vaccinated']]) and
             not any([s in col for s in ['Indigenous', 'Population', '- F -', '- M -']])
         ],
-        'age_5_11_2dose': [
+        'age 5-11, 2+ doses': [
             col for col in df.columns if
             'National' in col and
             '5-11' in col and
             any([s in col for s in ['2 doses', 'fully vaccinated']])
         ],
     }
-    masks['age_16_2dose'].sort()
+    masks['age 16+, 2+ doses'].sort()
     return masks
 
 
@@ -48,7 +48,7 @@ def add_booster_data_to_vacc(
     immune_duration: int,
 ) -> pd.DataFrame:
     masks = get_vacc_data_masks(df)
-    df['adult booster'] = df.loc[:, masks['age_16_3dose'] + masks['age_16_4dose']].sum(axis=1)
+    df['adult booster'] = df.loc[:, masks['age 16+, 3+ doses'] + masks['age 16+, 4+ doses']].sum(axis=1)
     df = df.drop(datetime(2022, 7, 8))
     df['adult booster smooth'] = df.loc[:, 'adult booster'].rolling(rolling_window).mean()
     df['incremental adult booster'] = df['adult booster smooth'].diff()
